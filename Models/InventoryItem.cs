cd6b310ee82710ce19d5819f31641fd81d6cf7a7
Jessamin Karabinovich <64698383+MashaKarabinovich@users.noreply.github.com>
2021-04-12 11:05:21 -0400

    Add files via upload

---
+++ Models/InventoryItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LinqApplication.Models
+{
+    public class InventoryItem
+    {
+        public string itemnum {get; set;} //to search for item num
+        public string item_type { get; set; } //moulding type
+    }
+}
